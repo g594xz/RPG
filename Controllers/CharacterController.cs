1f99863a847bc259f8e73fdbbfe5773c22435484
Alan Guo <Alan.Guo@vmlyr.com>
2021-09-26 01:15:54 +1200

    Basic character methods

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character{Id =1, Name = "Alan"}
+        }; 
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c=> c.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharater(Character character)
+        {
+            characters.Add(character);
+            return Ok(characters);
+        }
+    }
+}
